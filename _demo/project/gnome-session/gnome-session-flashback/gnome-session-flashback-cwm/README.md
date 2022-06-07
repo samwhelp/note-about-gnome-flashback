079a39124fcbb9e690b74f11da974ff6ef6897c1
samwhelp <samwhelp@users.noreply.github.com>
2022-06-07 20:37:15 +0800

    fix

---
+++ _demo/project/gnome-session/gnome-session-flashback/gnome-session-flashback-cwm/README.md
@@ -0,0 +1,68 @@
+
+# gnome-session-flashback-cwm
+
+## Main
+
+* [gnome-session-flashback-cwm](asset/gnome-session-flashback-cwm)
+
+
+## Usage
+
+### help
+
+run
+
+``` sh
+make
+```
+
+or run
+
+``` sh
+make help
+```
+
+
+### prepare
+
+run
+
+``` sh
+make prepare
+```
+
+
+### build
+
+run
+
+``` sh
+make build
+```
+
+
+### install
+
+run
+
+``` sh
+make install
+```
+
+
+### remove
+
+run
+
+``` sh
+make remove
+```
+
+
+### clean
+
+run
+
+``` sh
+make clean
+```
---
+++ _demo/project/gnome-session/gnome-session-flashback/gnome-session-flashback-cwm/asset/gnome-session-flashback-cwm/remove.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "sudo rm -f /usr/share/xsessions/gnome-flashback-cwm.desktop"
+sudo rm -f /usr/share/xsessions/gnome-flashback-cwm.desktop
+
+echo "sudo rm -f /usr/libexec/gnome-flashback-cwm"
+sudo rm -f /usr/libexec/gnome-flashback-cwm
+
+echo "sudo rm -f /usr/share/gnome-session/sessions/gnome-flashback-cwm.session"
+sudo rm -f /usr/share/gnome-session/sessions/gnome-flashback-cwm.session
+
+echo "sudo rm -f /usr/share/applications/gnome-flashback-cwm-session.desktop"
+sudo rm -f /usr/share/applications/gnome-flashback-cwm-session.desktop
+
+echo "sudo rm -f /usr/bin/gnome-flashback-cwm-session"
+sudo rm -f /usr/bin/gnome-flashback-cwm-session
