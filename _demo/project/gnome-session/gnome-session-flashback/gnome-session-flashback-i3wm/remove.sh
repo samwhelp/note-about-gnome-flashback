047612ccb25960b8f9d20f10819184c95ff7e62f
samwhelp <samwhelp@users.noreply.github.com>
2023-02-03 19:00:36 +0800

    fix

---
+++ _demo/project/gnome-session/gnome-session-flashback/gnome-session-flashback-i3wm/remove.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "sudo rm -f /usr/share/xsessions/gnome-flashback-i3.desktop"
+sudo rm -f /usr/share/xsessions/gnome-flashback-i3.desktop
+
+echo "sudo rm -f /usr/libexec/gnome-flashback-i3"
+sudo rm -f /usr/libexec/gnome-flashback-i3
+
+echo "sudo rm -f /usr/share/gnome-session/sessions/gnome-flashback-i3.session"
+sudo rm -f /usr/share/gnome-session/sessions/gnome-flashback-i3.session
+
+echo "sudo rm -f /usr/share/applications/gnome-flashback-i3-session.desktop"
+sudo rm -f /usr/share/applications/gnome-flashback-i3-session.desktop
+
+echo "sudo rm -f /usr/bin/gnome-flashback-i3-session"
+sudo rm -f /usr/bin/gnome-flashback-i3-session
---
+++ read/project/gnome-session-flashback/gnome-session-flashback-i3wm.md
@@ -0,0 +1,15 @@
+---
+title: gnome-session-flashback-i3wm
+nav_order: 8011
+has_children: false
+parent: gnome-session-flashback
+grand_parent: 專案
+---
+
+
+# gnome-session-flashback-i3wm
+
+
+## 專案連結
+
+* [gnome-session-flashback-i3wm](https://github.com/samwhelp/note-about-gnome-flashback/tree/gh-pages/_demo/project/gnome-session/gnome-session-flashback/gnome-session-flashback-i3wm)
